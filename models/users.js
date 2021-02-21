1460b938cd28265e7a33138ef7be97416df81e36
jesstoh <jesstoh23@gmail.com>
2021-02-21 22:01:36 +0800

    Create user schema

---
+++ models/users.js
@@ -0,0 +1,20 @@
+//DEPENDENCIES
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    img: String,
+    followers: [Schema.Types.ObjectId],
+    following: [Schema.Types.ObjectId],
+    email: String //Optional(to expand the feature)
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
