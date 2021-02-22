15f0ca55b975904783637f12d0b4d26d33908422
jesstoh <jesstoh23@gmail.com>
2021-02-22 22:08:10 +0800

    Login route working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -8,10 +8,33 @@ const User = require("../models/users.js");
 
 // New get - login page
 sessions.get("/login", (req, res) => {
-    res.render("sessions/new.ejs")
-})
+    res.render("sessions/new.ejs");
+});
 
-
-
+// Create post - user login
+sessions.post("/login", (req, res) => {
+    
+    User.findOne({ username: req.body.username }, (err, foundUser) => {
+        //If username does not exist
+        if (!foundUser) {
+            //Rendering login page with error message
+            res.render("sessions/new.ejs", { username: false });
+            // console.log("no user")
+        } else {
+            //Check if password is correct
+            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                req.session.currentUser = foundUser;
+                res.send("password is correct");
+                // console.log("success");
+                //redirect to home page if password is correct
+                // res.redirect("/home");
+            } else {
+                //Rendering login page with error message if incorrect password
+                res.render("sessions/new.ejs", { password: false });
+                // console.log("wrong password")
+            }
+        }
+    });
+});
 
 module.exports = sessions;
