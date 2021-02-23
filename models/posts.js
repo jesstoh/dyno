2b1cb61e57dfa5fe13bd3b2f3459f9eb71171c30
jesstoh <jesstoh23@gmail.com>
2021-02-23 20:54:28 +0800

    Change schema to add edited property

--- models/posts.js
+++ models/posts.js
@@ -4,18 +4,30 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const postSchema = new Schema(
-  {
-    author: {type: String, required: true},
-    // author: {type: Schema.Types.ObjectId, required: true},
-    description: String,
-    img: String,
-    video: String,
-    location: String,
-    tags: [String],
-    likes: [String], //use username instead of Schema.Types.ObjectId as user route is not by id
-    comments: [{userId: Schema.Types.ObjectId, user: String, message: String}]
-  },
-  { timestamps: true }
+    {
+        author: { type: String, required: true },
+        // author: {type: Schema.Types.ObjectId, required: true},
+        description: String,
+        img: String,
+        video: String,
+        location: String,
+        tags: [String],
+        likes: [String], //use username instead of Schema.Types.ObjectId as user route is not by id
+        edited: {type: Boolean, default: false},
+        comments: [
+            {   //Sub-document for comment
+                type: new Schema(
+                    {
+                        user: String,
+                        message: String,
+                    },
+                    { timestamps: true }
+                ),
+            },
+        ],
+        // comments: [{user: String, message: String}, {timestamps: true}]
+    },
+    { timestamps: true }
 );
 
 const Post = mongoose.model("post", postSchema);
