15f8d628fa04685f33763acd6db5a5618a037361
jesstoh <jesstoh23@gmail.com>
2021-02-25 22:50:10 +0800

    Show comments working

--- public/js/post_show.js
+++ public/js/post_show.js
@@ -1,7 +1,7 @@
 $(() => {
     // Show comment box when click on comment icon
     $("#comment").on("click", () => {
-        $(".comment-box").show();
+        $(".comment-box").toggle("display");
     });
 
     // Hide comment box when click on cancel button
@@ -9,7 +9,11 @@ $(() => {
         // Prevent refreshing and submit when clicking on cancel button
         event.preventDefault();
         $("textarea").val(""); //empty text in comment box
-        $(".comment-box").hide();
+        $(".comment-box").hide("slow");
 
     });
+
+    $(".comment-show").on("click", () => {
+        $(".main-comments-container").toggle("display");
+    })
 });
