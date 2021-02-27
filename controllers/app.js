c7fa2a2fa407a26d97957989aa12d94205b06d22
jesstoh <jesstoh23@gmail.com>
2021-02-27 16:10:42 +0800

    Search route working

--- controllers/app.js
+++ controllers/app.js
@@ -26,6 +26,7 @@ apps.get("/home", isAuthenticated, (req, res) => {
             res.render("app/index.ejs", {
                 currentUser: req.session.currentUser,
                 posts,
+                query: {},
             });
         });
 });
@@ -43,6 +44,32 @@ apps.get("/following", isAuthenticated, (req, res) => {
                 posts,
             });
         });
+});
+
+// Index - showing search result
+apps.get("/search", isAuthenticated, (req, res) => {
+    // If search input is empty string, redirect to home
+    if (!req.query.q.trim()) {
+        res.redirect("/home");
+    }
+
+    // Create query parameter and regex pattern to ignore case
+    const query = { [req.query.cat]: new RegExp(req.query.q, "i") };
+    Post.find(query)
+        .sort({ _id: -1 })
+        .exec((err, posts) => {
+            posts.forEach((post) => {
+                post.created = formatDate(post.createdAt);
+            });
+            res.render("app/index.ejs", {
+                currentUser: req.session.currentUser,
+                posts,
+                query: req.query,
+            });
+        });
+
+    // res.send()
+    // res.send(req.query)
 });
 
 // apps.get("/back", (req, res) => {
--- public/js/posts.js
+++ public/js/posts.js
@@ -1,9 +1,22 @@
 $(() => {
-
     $(".post-card .card-content").on("click", (event) => {
-        window.location = $(event.currentTarget).find(".post-link").attr("href")
+        window.location = $(event.currentTarget)
+            .find(".post-link")
+            .attr("href");
     });
 
     $(".materialboxed").materialbox();
 
-})
+    // Initialize select
+    
+
+    let query = JSON.parse(thisQuery);
+    const catMatch = { tags: "Tag", author: "User", location: "Place" };
+    console.log(query)
+
+    if (query) {
+        $("#q").val(query.q);
+        $(`#cat option[value=${query.cat}]`).prop("selected", true);
+    }
+    $("select").formSelect();
+});
