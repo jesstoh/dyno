a9fa5d7429dc6d35e02399a201ba5e14670ebc6c
jesstoh <jesstoh23@gmail.com>
2021-02-28 14:23:33 +0800

    Add animation to form

--- public/js/posts.js
+++ public/js/posts.js
@@ -7,15 +7,28 @@ $(() => {
 
     $(".materialboxed").materialbox();
 
-    // Initialize select
-
     let query = JSON.parse(thisQuery);
     console.log(query);
 
     if (query) {
         $("#q").val(query.q);
         $(`#cat option[value=${query.cat}]`).prop("selected", true);
-    } 
-
+    }
+    // Initialize select
     $("select").formSelect();
+
+    // Change select option color
+    $(".cat-select ul li span").addClass("blue-text");
+
+
+    // Event listener when scroll to bottom
+    $(window).on("scroll", event => {
+
+        if (($(window).innerHeight() + $(window).scrollTop() + 30) > $("body").innerHeight()) {
+            console.log("end of page")
+        }
+    })
+
+
+
 });
